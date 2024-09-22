f264cee0eb8fe695ca3051ba881f21a6b4c98da2
ethnimf <144279943+ethnimf@users.noreply.github.com>
2024-09-22 13:16:37 +0300

    meow

---
+++ WpfApp14/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using SerializationLibrary1;  
+
+namespace WpfApp14
+{
+    public partial class MainWindow : Window
+    {
+        private Serializer _serializer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _serializer = new Serializer();
+        }
+
+    }
+}
